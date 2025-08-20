efd178790708a60088224e1033ae0e5367fba9cc
sharath44665 <sharathwelcomesyouin@gmail.com>
2025-08-20 20:49:06 +0530

    add text field with icon

--- currency_converter/lib/currency_converter_material_page.dart
+++ currency_converter/lib/currency_converter_material_page.dart
@@ -5,6 +5,40 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text("Hello world")));
+    final border = OutlineInputBorder(
+      borderSide: BorderSide(width: 2.0, style: BorderStyle.solid),
+      borderRadius: BorderRadius.circular(5),
+    );
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              "0",
+              style: TextStyle(
+                fontSize: 45,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 255, 255, 255),
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Please enter the amount in USD",
+                hintStyle: const TextStyle(color: Colors.black),
+                prefixIcon: Icon(Icons.monetization_on),
+                prefixIconColor: Colors.black,
+                filled: true,
+                fillColor: Colors.white,
+                focusedBorder: border,
+                enabledBorder: border,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
