dd77ddd86b17f40451947e2a724003d69a37a0a2
sharath44665 <sharathwelcomesyouin@gmail.com>
2025-08-20 18:13:00 +0530

    ignore warning by implementing required methods

--- currency_converter/lib/main.dart
+++ currency_converter/lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return Text("hello world", textDirection: TextDirection.ltr);
+    return const Text("hello world", textDirection: TextDirection.ltr);
   }
 }
