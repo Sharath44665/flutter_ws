ab92f31e51b41927a7c6b6de98fa068425d3855e
sharath44665 <sharathwelcomesyouin@gmail.com>
2025-09-20 18:58:42 +0530

    adjust padding, bgColor, borderRadius for shoe details

--- n06_shop_app/lib/product_card.dart
+++ n06_shop_app/lib/product_card.dart
@@ -14,14 +14,20 @@ class ProductCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Color.fromRGBO(216, 240, 253, 1),
+      margin: const EdgeInsets.all(20),
+      padding: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Color.fromRGBO(216, 240, 253, 1),
+      ),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(title, style: Theme.of(context).textTheme.titleMedium),
           const SizedBox(height: 5),
           Text("₹$price", style: Theme.of(context).textTheme.bodySmall),
           const SizedBox(height: 5),
-          Image.asset(image, height: 175),
+          Center(child: Image.asset(image, height: 175)),
         ],
       ),
     );
